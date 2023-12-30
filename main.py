9e1551d01c7d1a05ad48910d54c8fb69b81cc579
Giridharj <giridharjakkampudi.123@gmail.com>
2023-12-30 14:45:10 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,32 @@
+"""Please visit the website to run the code"""
+
+"""https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=problem_world2.json&url=user_world%3Aproblem_world2.json"""
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+while at_goal() != True:
+    if right_is_clear()!= True:
+        while right_is_clear()!= True and at_goal()!= True:
+            while wall_in_front() != True and at_goal()!= True:
+                if right_is_clear()!= True and at_goal()!=True:
+                    move()
+                else:
+                    turn_right()
+                    if wall_in_front != True and at_goal()!=True:
+                        move()                  
+            if right_is_clear()== True and at_goal()!=True:
+                turn_right()
+                if wall_in_front != True:
+                    move()
+            else:
+                turn_left()
+    else:
+        turn_right()
+        while front_is_clear() == True:
+            if at_goal()== True:
+                break
+            move()
+        
